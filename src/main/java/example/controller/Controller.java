f2ef3b8a3073e336b914351f9494cffbd8cb3f57
Francisco <franciscomarinalvarez@hotmail.es>
2024-10-24 09:17:56 +0200

    Fxml load 50%

---
+++ src/main/java/example/controller/Controller.java
@@ -0,0 +1,17 @@
+package example.controller;
+
+import example.App;
+
+import java.io.IOException;
+
+public abstract class Controller {
+    App app;
+
+    public void setApp(App app) {
+        this.app = app;
+    }
+
+    public abstract void onOpen(Object input) throws IOException;
+
+    public abstract void onClose(Object output);
+}
