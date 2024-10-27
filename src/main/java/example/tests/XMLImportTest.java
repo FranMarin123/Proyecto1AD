568507b20ea66f0bcd8443051bbf6bd5934ca7a3
Francisco <franciscomarinalvarez@hotmail.es>
2024-10-28 00:12:08 +0100

    Register

--- src/main/java/example/tests/XMLImportTest.java
+++ src/main/java/example/tests/XMLImportTest.java
@@ -10,6 +10,17 @@ import java.util.List;
 public class XMLImportTest {
     public static void main(String[] args) {
         UserCollection ejemplo=XMLManager.readXML(new UserCollection(),"usuarios.xml");
-        System.out.println(ejemplo);
+        User usuario=new User("","juan@gmail.com","1234");
+        System.out.println(browseUserInArray(ejemplo,usuario));
+    }
+
+    public static User browseUserInArray(UserCollection allUsers,User userToBrowse){
+        User userReturn=null;
+        for (int i=0;i<allUsers.getUsers().size();i++){
+            if (allUsers.getUsers().get(i).equals(userToBrowse)){
+                userReturn=allUsers.getUsers().get(i);
+            }
+        }
+        return userReturn;
     }
 }
